4f9021b8e932988a9606413d459e0d03e773f744
Michele_Manco <motlamik@gmail.com>
2020-02-13 20:33:22 +0100

    footer mod

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer mt-auto py-3">
+<footer class="footer mt-auto py-3 fixed-bottom">
   <div class="container  text-center">
     <span class="text-muted">  logo boolean + menu + copyright e link social</span>
   </div>
