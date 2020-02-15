07143e95cb417e9be8e308e8cd4114358e4081bc
Michele_Manco <motlamik@gmail.com>
2020-02-15 18:42:14 +0100

    fav-icon added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Boolean Careers</title>
+    <link rel="shortcut icon" href="https://www.boolean.careers/images/favicon/favicon.ico" type="image/x-icon">
     <script src="https://kit.fontawesome.com/e23757acf7.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js')}}" charset="utf-8"></script>
